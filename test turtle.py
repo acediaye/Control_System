307aef0e56991fc36bb34b0f9d5a6683c68a6e8f
Andy <ayeh616@gmail.com>
2021-06-06 21:18:11 -0400

    test turtle

--- test turtle.py
+++ test turtle.py
@@ -8,4 +8,27 @@ print(turtle.ycor())
 
 turtle.left(180)
 
+p1 = turtle.Turtle()
+p1.color('green')
+p1.shape('turtle')
+p1.penup()
+p1.goto(-200, 100)
+
+p2 = p1.clone()
+p2.color('blue')
+p2.penup()
+p2.goto(-200, -100)
+
+p1.goto(300, 100)
+p1.pendown()
+p1.circle(40)
+p1.penup()
+p1.goto(-200, 100)
+
+p2.goto(300, -100)
+p2.pendown()
+p2.circle(40)
+p2.penup()
+p2.goto(-200, -100)
+
 turtle.done()
